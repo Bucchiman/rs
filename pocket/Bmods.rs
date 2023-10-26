611613d91fb8cde581c75f8dda87b5627bd0e6af
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-10-26 18:33:05 +0900

    update modules

--- pocket/Bmods.rs
+++ pocket/Bmods.rs
@@ -12,7 +12,6 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-
 fn fileIO () {
     let mut f = File::open("./poem.txt").expect("file not found");
     let mut contents = String::new();
@@ -23,9 +22,63 @@ fn fileIO () {
 //let mut f = File::open(filename).expect("file not found");
 
 
+
+/// Input string to 
+///
+/// # Arguments
+///
+/// * `name` - A string slice that holds the name of the person
+///
+/// # Examples
+///
+/// ```
+/// // You can have rust code between fences inside the comments
+/// // If you pass --test to `rustdoc`, it will even test it for you!
+/// use doc::Person;
+/// let person = Person::new("name");
+/// ```
+/// Reference: https://doc.rust-jp.rs/book-ja-pdf/book.pdf
+///
+fn inputIO () {
+    use std::io;
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+}
+
+/// Returns a person with the name given them
+///
+/// # Arguments
+///
+/// *  - A string slice that holds the name of the person
+///
+/// # Examples
+///
+/// rustdoc
+/// Reference: https://doc.rust-jp.rs/book-ja-pdf/book.pdf
+///
+fn guess_number () {
+    use std::io;
+
+    let mut value = String::new();
+    io::stdin().read_line(&mut value).expect("Failed to read line");
+}
+
+
+//
+// tips
+// :: ... expect associative function
+//
+// e.g.
+//      String::new()   "String" expects "new" function
+//
+//
+//
+//
+
+
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    fileIO();
+    // fileIO();
     Ok(())
 }
